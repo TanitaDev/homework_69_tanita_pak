1311d2776f054532b60a29d4dc90362b62a5be82
tanitaDev <tanita.pak@gmail.com>
2022-11-18 14:19:04 +0600

    написала view для деления чисел

--- app/webapp/views/views.py
+++ app/webapp/views/views.py
@@ -46,4 +46,19 @@ def multiply_view(request, *args, **kwargs):
     return response
 
 
-
+def divide_view(request, *args, **kwargs):
+    data = {
+        'A': 1234,
+        'B': 4567,
+    }
+    if type(data['A']) != int or type(data['B']) != int:
+        response = JsonResponse({"error": "Not integer"})
+        response.status_code = 400
+        return response
+    elif data['B'] == 0:
+        response = JsonResponse({"error": "Division by zero"})
+        response.status_code = 400
+        return response
+    else:
+        response = JsonResponse({'answer': data['A'] / data['B']})
+    return response
