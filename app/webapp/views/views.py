094c68e7b8db96f1905aec324d955c78e8ebc10b
tanitaDev <tanita.pak@gmail.com>
2022-11-18 14:16:43 +0600

    написала view для вычитания и умножения чисел

--- app/webapp/views/views.py
+++ app/webapp/views/views.py
@@ -18,3 +18,32 @@ def add_view(request, *args, **kwargs):
     return response
 
 
+def subtract_view(request, *args, **kwargs):
+    data = {
+        'A': 1234,
+        'B': 4567,
+    }
+    if type(data['A']) != int or type(data['B']) != int:
+        response = JsonResponse({"error": "Not integer"})
+        response.status_code = 400
+        return response
+    else:
+        response = JsonResponse({'answer': data['A'] - data['B']})
+    return response
+
+
+def multiply_view(request, *args, **kwargs):
+    data = {
+        'A': 1234,
+        'B': 4567,
+    }
+    if type(data['A']) != int or type(data['B']) != int:
+        response = JsonResponse({"error": "Not integer"})
+        response.status_code = 400
+        return response
+    else:
+        response = JsonResponse({'answer': data['A'] * data['B']})
+    return response
+
+
+
